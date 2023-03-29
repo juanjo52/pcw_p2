51c453cec92e36fd4651ff8b223a103bbd341e28
juanjo52 <juanjo8marzo@gmail.com>
2023-03-29 16:31:28 +0200

    Ejemplo 5 Profesor - Tratamiento formularios (2)

--- ejemplos_clase/ejemplo5/codigo5.js
+++ ejemplos_clase/ejemplo5/codigo5.js
@@ -1,8 +1,8 @@
-(function(){
-    if(!sessionStorage['_datos_']){
-        location.href = 'index.html';
-    }
-})(); // esta función se llama a si misma
+// (function(){
+//     if(!sessionStorage['_datos_']){
+//         location.href = 'index.html';
+//     }
+// })(); // esta función se llama a si misma
 
 function hacerLogin(evt){
     evt.preventDefault(); // cancela la acción por defecto del evento
@@ -48,4 +48,48 @@ function cerrarDialogo(valor){
     console.log(valor);
     document.querySelector('dialog').close();
     document.querySelector('dialog').remove();
+}
+
+function hacerComentario_xhr(frm){
+    let xhr = new XMLHttpRequest(),
+        url = 'api/publicaciones/2/comentarios',
+        fd = new FormData(frm),
+        usu = JSON.parse( sessionStorage['_datos_']),
+        auth;
+    
+    xhr.open('POST', url, true);
+    xhr.responseType = 'json';
+
+    xhr.onload = function(){
+        let r = xhr.response;
+
+        console.log(r);
+    }
+    auth = usu.LOGIN + ':' + usu.TOKEN;
+    xhr.setRequestHeader('Authoritation', auth);
+    xhr.send(fd);
+
+    return false; // cancela la acción por defecto del evento
+}
+
+function hacerComentario(frm){
+    let url = 'api/publicaciones/2/comentarios',
+        fd = new FormData(frm), 
+        usu = JSON.parse( sessionStorage['_datos_']),
+        auth;
+    
+    auth = usu.LOGIN + ':' + usu.TOKEN;
+    fetch(url, {method:'POST',
+                body: FileSystem,
+                headers: {'Authoritation': auth}}).then(function(response){
+        if(response.ok){
+            response.json().then(function(datos){
+                console.log(datos);
+            })
+        }
+    }).catch(function(error){
+        console.log(error);
+    });
+
+    return false;
 }
