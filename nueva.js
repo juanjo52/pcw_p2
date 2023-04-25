1d1777af08d089230bc2ad89e6e492aceec9a8d8
juanjo52 <juanjo8marzo@gmail.com>
2023-04-25 19:41:34 +0200

    Actualizado logout en nav en todos los js

---
+++ nueva.js
@@ -0,0 +1,26 @@
+function muestraNav() {
+    let ul = document.createElement("ul");
+    let userName = '';
+    let datos;
+
+    ul.classList.add('enlaces');
+
+    if (sessionStorage['_datos_']) {
+        datos = JSON.parse(sessionStorage['_datos_']);
+        userName = datos.NOMBRE;
+        console.log(userName);
+        ul.innerHTML = '<li><a href="./index.html" class="icon-home"><span>Inicio</span></a></li>' +
+            '<li><a href="./buscar.html" class="icon-search"><span>Buscar</span></a></li>' +
+            '<li><a href="./nueva.html" class="icon-plus"><span>Nueva</span></a></li>' +
+            '<li><a href="./index.html" onclick="logout();" class="icon-logout"><span>'+userName+'</span></a></li>';
+
+        document.querySelector('#menuNav').appendChild(ul);
+    } else {
+        ul.innerHTML = '<li><a href="./index.html" class="icon-home"><span>Inicio</span></a></li>' +
+            '<li><a href="./buscar.html" class="icon-search"><span>Buscar</span></a></li>' +
+            '<li><a href="./login.html" class="icon-login"><span>Login</span></a></li>' +
+            '<li><a href="./registro.html" class="icon-user-add"><span>Registro</span></a></li>';
+
+        document.querySelector('#menuNav').appendChild(ul);
+    }
+}
