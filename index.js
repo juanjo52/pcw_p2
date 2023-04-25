1bf06ab24b960e97ee6b36799944fde2a4ced49f
juanjo52 <juanjo8marzo@gmail.com>
2023-04-25 19:38:11 +0200

    Ahora en vez de logout, dice nombre de usuario iniciado

--- index.js
+++ index.js
@@ -96,13 +96,19 @@ function updateItems() {
 
 function muestraNav() {
     let ul = document.createElement("ul");
+    let userName = '';
+    let datos;
+
     ul.classList.add('enlaces');
 
     if (sessionStorage['_datos_']) {
+        datos = JSON.parse(sessionStorage['_datos_']);
+        userName = datos.NOMBRE;
+        console.log(userName);
         ul.innerHTML = '<li><a href="./index.html" class="icon-home"><span>Inicio</span></a></li>' +
             '<li><a href="./buscar.html" class="icon-search"><span>Buscar</span></a></li>' +
             '<li><a href="./nueva.html" class="icon-plus"><span>Nueva</span></a></li>' +
-            '<li><a href="./index.html" onclick="logout();" class="icon-logout"><span>Logout</span></a></li>';
+            '<li><a href="./index.html" onclick="logout();" class="icon-logout"><span>'+userName+'</span></a></li>';
 
         document.querySelector('#menuNav').appendChild(ul);
     } else {
