dab397f7d6392b51db30d2e7b1661584122ddda6
juanjo52 <juanjo8marzo@gmail.com>
2023-04-26 22:04:57 +0200

    Paginación completa (https://youtu.be/twstZoZLG-k)

--- index.js
+++ index.js
@@ -1,107 +1,80 @@
-const PAGE_SIZE = 2;
-let currentPage = 0;
+const PAGE_SIZE = 1; //CAMBIAR A 6 O LO QUE VEAMOS
 let totalItems;
+let currentPage;
 
-console.log("Current Page: "+ currentPage);
+function getPage() {
+    let pagActual = document.querySelector('#current-page');
+    let numPagActual = pagActual.textContent;
 
-function getTotalItems() {
-    let url = 'api/publicaciones';
-    fetch(url).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (datos) {
-                console.log(datos);
-                totalItems = datos.FILAS.length;
-                console.log("Total Items: "+ totalItems);
-                updatePagination();
-                updateItems();
-            });
-        }
-    });
+    return Number(numPagActual);
 }
 
-getTotalItems();
+function getFinal() {
+    let totalPags = document.querySelector('#total-pages');
+    let numPagsTotal = totalPags.textContent;
 
-function updatePagination() {
-    const totalPages = Math.ceil(totalItems / PAGE_SIZE);
-    console.log("Total Pages: "+ totalPages);
-    console.log("Current Page: "+ currentPage);
+    return Number(numPagsTotal);
+}
 
-    // Disable/enable buttons as needed
-    const firstPageButton = document.querySelector('#first-page');
-    const prevPageButton = document.querySelector('#prev-page');
-    const nextPageButton = document.querySelector('#next-page');
-    const lastPageButton = document.querySelector('#last-page');
-    firstPageButton.disabled = currentPage == 0;
-    prevPageButton.disabled = currentPage == 0;
-    nextPageButton.disabled = currentPage == totalPages - 1;
-    lastPageButton.disabled = currentPage == totalPages - 1;
-
-    // Update page number display
-    let currentPageDisplay = document.querySelector('#current-page');
-    let totalPageDisplay = document.querySelector('#total-pages');
-    currentPageDisplay.textContent = currentPage + 1;
-    totalPageDisplay.textContent = totalPages;
-
-    // Add click event listeners to buttons
-    firstPageButton.removeEventListener('click', firstPageClickHandler);
-    prevPageButton.removeEventListener('click', prevPageClickHandler);
-    nextPageButton.removeEventListener('click', nextPageClickHandler);
-    lastPageButton.removeEventListener('click', lastPageClickHandler);
-
-    firstPageButton.addEventListener('click', firstPageClickHandler);
-    prevPageButton.addEventListener('click', prevPageClickHandler);
-    nextPageButton.addEventListener('click', nextPageClickHandler);
-    lastPageButton.addEventListener('click', lastPageClickHandler);
-
-    function firstPageClickHandler() {
+function updateItems(principio, unidades, final) {
+    if(principio){
         currentPage = 0;
-        updateItems();
-        updatePagination();
     }
 
-    function prevPageClickHandler() {
-        if (currentPage > 0) {
-            currentPage--;
-            updateItems();
-            updatePagination();
-        }
+    if(!principio && !final){
+        currentPage = getPage()-1;
+        console.log(currentPage);
+        currentPage += unidades;
     }
 
-    function nextPageClickHandler() {
-        if (currentPage < totalPages - 1) {
-            currentPage++;
-            updateItems();
-            updatePagination();
-        }
+    if(final){
+        currentPage = getFinal()-1;
     }
-
-    function lastPageClickHandler() {
-        currentPage = totalPages - 1;
-        updateItems();
-        updatePagination();
-    }
-}
-
-
-function updateItems() {
+    
     let url = 'api/publicaciones';
 
-    url = url + '?pag=${currentPage}&lpag=${PAGE_SIZE}';  //pag nº pagina, lpag cuantos elementos hay en la página
+    
+    // url = url + '?pag=${currentPage}&lpag=${PAGE_SIZE}';  //pag nº pagina, lpag cuantos elementos hay en la página
+    let pag = currentPage;
+    url = url + '?pag='+pag+'&lpag='+PAGE_SIZE;
 
     const elementoPadre = document.getElementById('contenedorPublicaciones');
 
-    
-
     fetch(url).then(function (response) {
-        console.log(elementoPadre);
+        // console.log(elementoPadre);
         if (elementoPadre) { //Esto es para evitar repeticiones al hacer varias veces el onclick
-            while (elementoPadre.firstChild) {
-            elementoPadre.removeChild(elementoPadre.firstChild);
-            }
+            elementoPadre.innerHTML = ''
         }
+
         if (response.ok) {
+
             response.json().then(function (datos) {
+                
                 console.log(datos);
+                totalItems = datos.TOTAL_COINCIDENCIAS;
+              
+                const totalPages = Math.ceil(totalItems / PAGE_SIZE);
+
+                    /****/ 
+                // Disable/enable buttons as needed
+                const firstPageButton = document.querySelector('#first-page');
+                const prevPageButton = document.querySelector('#prev-page');
+                const nextPageButton = document.querySelector('#next-page');
+                const lastPageButton = document.querySelector('#last-page');
+                firstPageButton.disabled = pag == 0;
+                prevPageButton.disabled = pag == 0;
+                nextPageButton.disabled = pag == totalPages - 1;
+                lastPageButton.disabled = pag == totalPages - 1;
+
+                // Update page number display
+                let currentPageDisplay = document.querySelector('#current-page');
+                let totalPageDisplay = document.querySelector('#total-pages');
+                currentPageDisplay.textContent = pag + 1;
+                totalPageDisplay.textContent = totalPages;
+                /****/ 
+
+
+                
                 datos.FILAS.forEach(function (e) {
                     let article = document.createElement('article');
 
@@ -119,8 +92,10 @@ function updateItems() {
 
                     document.querySelector('#contenedorPublicaciones').appendChild(article);
 
-
                 });
+                
+
+                
             });
         }
     }).catch(function (error) {
@@ -128,9 +103,6 @@ function updateItems() {
     });
 
 }
-
-
-
 
 function muestraNav() {
     let ul = document.createElement("ul");
@@ -142,7 +114,7 @@ function muestraNav() {
     if (sessionStorage['_datos_']) {
         datos = JSON.parse(sessionStorage['_datos_']);
         userName = datos.NOMBRE;
-        console.log(userName);
+        // console.log(userName);
         ul.innerHTML = '<li><a href="./index.html" class="icon-home"><span>Inicio</span></a></li>' +
             '<li><a href="./buscar.html" class="icon-search"><span>Buscar</span></a></li>' +
             '<li><a href="./nueva.html" class="icon-plus"><span>Nueva</span></a></li>' +
@@ -163,8 +135,6 @@ function logout() {
     if (sessionStorage['_datos_']) {
         sessionStorage.removeItem("_datos_");
     }
-
-    console.log("gola");
 }
 
 
