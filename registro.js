1803b6a518b550414c1a6dd94a93e40649f5b15b
juanjo52 <juanjo8marzo@gmail.com>
2023-04-27 10:57:08 +0200

    Comprobación en registro si username ya existe o no

--- registro.js
+++ registro.js
@@ -4,6 +4,8 @@
     }
 })();
 
+
+
 function muestraNav() {
     let ul = document.createElement("ul");
     let userName = '';
@@ -30,3 +32,50 @@ function muestraNav() {
         document.querySelector('#menuNav').appendChild(ul);
     }
 }
+
+
+//COMPROBAR QUE EL USERNAME (LOGIN) NO ESTÁ YA EN LA BBDD
+let login = document.getElementById('login');
+
+login.addEventListener("blur", function(evt) {
+    compruebaDisponibleLogin();
+});
+
+function compruebaDisponibleLogin() {
+    let spanMensaje = document.getElementById('msgDisponible');
+    let url = 'api/usuarios/'
+    let loginIntroducido = login.value;
+    url = url + loginIntroducido;
+    let disponible;
+
+    fetch(url).then(function(response){
+        if(response.ok){
+            response.json().then(function(datos){
+                disponible = datos.DISPONIBLE;
+
+                if(disponible){
+                    if(spanMensaje.classList.contains('no')){
+                        spanMensaje.classList.remove('no');
+                    }
+
+                    spanMensaje.classList.add('si');
+                    spanMensaje.textContent = 'Este login está disponible :D';
+                }else {
+                    if(spanMensaje.classList.contains('si')){
+                        spanMensaje.classList.remove('si');
+                    }
+
+                    spanMensaje.classList.add('no');
+                    spanMensaje.textContent = 'Este login no está disponible :(';
+                }
+            });
+        }else {
+            console.log("not ok");
+        }
+    }).catch(function(error){
+        console.log(error);
+    });
+}
+
+
+
