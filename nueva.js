dfee163860a7e638a99e1f781ddd375904277b9d
fvp10 <el18delacross@gmail.com>
2023-04-30 10:44:46 +0200

    muestraIMG nueva

--- nueva.js
+++ nueva.js
@@ -66,13 +66,52 @@ function crearImagenes(){
     d.classList.add('imagenes');
     
     d.innerHTML = 
-    '<label for = "foto'+num+'"><img src="./img/no-image.png" alt="Imagen '+num+'"></label>'+
+    '<label for = "foto'+num+'"><img src="./img/no-image.png" alt="Imagen '+num+'" id ="img'+num+'"></label>'+
         '<div class="button-group">'+
             '<label for="foto'+num+'" class="icon-folder-add"></label>'+
-            '<input type="file" id="foto'+num+'" accept="image/*" name ="foto[]">'+
-            '<button class="icon-folder-delete"></button>'+
+            '<input type="file" id="foto'+num+'" accept="image/*" name ="foto[]" onchange="mostrarFoto(this)">'+
+            '<button class="icon-folder-delete" onclick="eliminarFoto()></button>'+
+            '<span id ="avisoFoto"></span>'+
         '</div>';
 
     document.querySelector('#fotos').appendChild(d);    
     console.log(num);
+}
+
+
+// Tamaño maximo imagen
+const maxSize = 300000; //Bytes = 300 KB
+
+// PARTE IMAGENES
+
+function mostrarFoto(inp){
+    let fichero = inp.files[0]/*,
+        img = inp.parentElement.querySelector('img')*/;
+    let cont = document.querySelector('#contador');
+    let aux = Number(cont.textContent); 
+    let cID = "img"+aux;
+
+    console.log(cID);
+    
+    let fotoUsu = document.getElementById(cID);
+
+    console.log(fotoUsu);
+
+    if(fichero.size > maxSize){
+        let avisoFoto = document.getElementById('avisoFoto');
+        avisoFoto.textContent = 'El tamaño máximo para la foto son 300 KB'
+        avisoFoto.classList.add('no');
+        fotoUsu.src = './img/no-image.png';
+
+    } else {
+        fotoUsu.src = URL.createObjectURL( fichero );
+        avisoFoto.classList.remove('no');
+        avisoFoto.textContent = ''
+    }
+}
+
+function eliminarFoto(){
+    if(!fotoUsu.src.includes("no-image")){
+        fotoUsu.src = './img/no-image.png';
+    }
 }
